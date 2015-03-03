b96966891bb98f6f806cbb0729b76390b8d095bd
deltacat <deltacat@qq.com>
2015-03-03 15:56:18 +0800

    Study to chapter 6

--- src/chapter06/LottoNumbers.java
+++ src/chapter06/LottoNumbers.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class LottoNumbers {
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
-		boolean[] isCovered = new boolean[99]; // default false
+		int totalNumber = 9;
+		boolean[] isCovered = new boolean[totalNumber]; // default false
 
 		// Read all numbers and mark corresponding element covered
 		int number = input.nextInt();
@@ -13,10 +14,11 @@ public class LottoNumbers {
 			isCovered[number - 1] = true;
 			number = input.nextInt();
 		}
+		input.close();
 
 		// Check if all covered
 		boolean allCovered = true; // Assume all covered
-		for (int i = 0; i < 99; i++)
+		for (int i = 0; i < totalNumber; i++)
 			if (!isCovered[i]) {
 				allCovered = false; // Find one number is not covered
 				break;
@@ -26,6 +28,6 @@ public class LottoNumbers {
 		if (allCovered)
 			System.out.println("The tickets cover all numbers");
 		else
-			System.out.println("The tickets don’t cover all numbers");
+			System.out.println("The tickets don't cover all numbers");
 	}
 }
